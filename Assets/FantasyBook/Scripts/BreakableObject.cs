f2c8047483a996225d82ab5b21f1209b9987f5ba
Barsan Patricia <barsanpatricia@gmail.com>
2026-02-03 22:12:38 +0200

    improved dialogue system with choices, first draft for breakable object

---
+++ Assets/FantasyBook/Scripts/BreakableObject.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int hitsRequired = 5;
+    [SerializeField] private Sprite brokenSprite;
+
+    private int hits;
+    private SpriteRenderer spriteRenderer;
+    private bool isBroken;
+
+    private void Awake()
+    {
+        hits = hitsRequired;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public bool CanInteract()
+    {
+        return !isBroken;
+    }
+
+    public void Interact()
+    {
+        if (isBroken)
+            return;
+
+        hits--;
+
+        if (hits <= 0)
+        {
+            Break();
+        }
+    }
+
+    private void Break()
+    {
+        isBroken = true;
+        spriteRenderer.sprite = brokenSprite;
+    }
+}
---
+++ Assets/FantasyBook/Scripts/Managers/InputManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DefaultExecutionOrder(-1)]
+public class InputManager : Singleton<InputManager>
+{
+    public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+    public delegate void EndTouchEvent(Vector2 position, float time);
+    public event EndTouchEvent OnEndTouch;
+
+    private TouchControl touchControl;
+
+    private void Awake()
+    {
+        touchControl = new TouchControl();
+    }
+
+    private void OnEnable()
+    {
+        touchControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        touchControl.Disable();
+    }
+
+    private void Start()
+    {
+        /*Subscribe to an event (starting to touch) and pass the information from that event*/
+        touchControl.Touch.TouchPress.started += context => StartTouch(context);
+        touchControl.Touch.TouchPress.canceled += context => EndTouch(context);
+    }
+
+    private void StartTouch(InputAction.CallbackContext context)
+    {
+        if (OnStartTouch != null)
+        {
+            OnStartTouch(touchControl.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.startTime);
+        }
+    }
+
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+        if (OnEndTouch != null)
+        {
+            OnEndTouch(touchControl.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.time);
+        }
+    }
+}
